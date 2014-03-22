3834a3ac06237c7d0db047d31afc21e36fdad2c8
Kaelan Fouwels <kfouwels@outlook.com>
2014-03-22 13:11:08 +0000

    mongo

--- index.php
+++ index.php
@@ -2,25 +2,58 @@
 	require("toro.php");
 	require("apiKeys.php");
 
-	Toro::serve(array(
-		"/" => "RootHandler"
-		"/companies:company" =? "CompaniesHandler"
+	 $mon = new Mongo(getenv("MONGOLAB_URI"));
+	 $db = $mon->msomb;
 
+	 $collection = $db->companies;
+
+	Toro::serve(array(
+		"/" => "RootHandler",
+		"/companies/" => "Companies",
+		"/companies/socialData/:company" => "CompaniesSocial",
 	));
 
 	// /
 	Class RootHandler{
+		function get() {
+			echo "Endpoints:\n\n";
+			echo "/ => Root\n";
+
+			echo "Current time:".time()."<br/>";
+		}
+	}
+
+	// /companies
+	class Companies{
+		function get() {
+			//return all companies
+			echo "##Companies";
+			$companiesDatas = $collection->find();
+			echo "comp".$companiesDatas;
+		}
+	}
+	
+	// /companies/socialData/:company
+	class CompaniesSocial{
+		function get(){
+			//return socialData for :company#
+			$companiesDatas = $collection->find();
+			echo "comp".$companiesDatas;
+		}
+	}
+
+	class Twitter {
+
+		function authenticate(){
+
+		}
 
 	}
 
-	// /companies:company
+	class DBInteract{
+		function retrieveSocialNews(){
 
-	class CompaniesHandler{
-
+		}
 	}
-
-
-
-
 
 ?>
